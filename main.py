95eba2aa3cc9d6d14198f49029bf1e450b8f609b
brandonb123456 <133682239+brandonb123456@users.noreply.github.com>
2023-06-06 15:46:31 +0000

    I added the first lines of code. I imported libraries and put the test code.

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print ("Hello")
+import requests
+import json
+
+URL = "https://pokeapi.co/"
+print(f"Calling{URL}")
+response = requests.get(URL)
+response.raise_for_status()
+data = response.json()
+print(f"\nText returned: {response.text}")

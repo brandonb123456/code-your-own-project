ab01978d72d936a68e13d87a869200648d195a3e
andrewjd2005 <133681554+andrewjd2005@users.noreply.github.com>
2023-06-07 03:51:57 +0000

    Trying to print ability names

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ response = requests.get(URL)
 response.raise_for_status()
 data = response.json()
 
+response = requests.get("https://pokeapi.co/api/v2/pokemon/ditto/")
+attributes = response.json()
+# print(dir(attributes))
+# print(help(attributes))
+print(attributes.get("abilities"))
+
 ## Andrew will code above this line, Brandon will code below this code ##
 
 print("hello world")
